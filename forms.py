fa4e48f8a0f1a0279fcea6641c146d3db896dc59
Job <Jobsjobbema@gmail.com>
2026-03-29 20:01:03 +0200

    Validatie informatie toegevoed aan leeftijd en bio, zodat je niet oneindig kan typen

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField, IntegerField, SelectField, TextAreaField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, number_range
 
 class LoginForm(FlaskForm):
     email = EmailField('Email')
@@ -11,7 +11,7 @@ class RegistratieForm(FlaskForm):
     email = EmailField('Email')
     password = PasswordField('Wachtwoord')
     confirm_password = PasswordField('Herhaal wachtwoord', validators=[EqualTo('password', message='Wachtwoorden moeten overeenkomen!')])
-    leeftijd = IntegerField('Leeftijd')
+    leeftijd = IntegerField('Leeftijd', validators=[InputRequired(), number_range(min=18, max=100)])
     geslacht = SelectField(
         'Geslacht',
         choices=[
@@ -20,7 +20,7 @@ class RegistratieForm(FlaskForm):
             ('anders', 'Anders')
         ]
     )
-    bio = TextAreaField('Bio')
+    bio = TextAreaField('Bio', validators=[Length(min=10, max=251)])
 
     submit = SubmitField('Registreren', render_kw={"class":"btn btn-success"})
 
